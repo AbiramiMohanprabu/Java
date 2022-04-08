4d6aff1ba9852d5ec207029f472f2e5b3a01f9f7
AbiramiMohanprabu <102943522+AbiramiMohanprabu@users.noreply.github.com>
2022-04-08 10:57:29 +0530

    Add files via upload

---
+++ Practical/Whileexample.java
@@ -0,0 +1,18 @@
+class Whileexample{
+
+public void show(){
+
+int a=1;
+
+while(a<=20)
+{
+System.out.println("Value is :" +a);
+a++;
+}
+}
+public static void main(String args[]){
+Whileexample m = new Whileexample();
+
+m.show();
+}
+}
