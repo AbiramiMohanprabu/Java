4c0bd625581f44c788b98eb83ca1935b718a9f0b
AbiramiMohanprabu <102943522+AbiramiMohanprabu@users.noreply.github.com>
2022-04-08 10:52:32 +0530

    Add files via upload

---
+++ Practical/Forloopexample.java
@@ -0,0 +1,15 @@
+class Forloopexample{
+
+public void display(){
+	
+
+for( int i =1; i<=10; i++)
+{
+System.out.println("Value is :" +i);
+}
+}
+public static void main (String args[]){
+Forloopexample m = new Forloopexample();
+m.display();
+}
+}
