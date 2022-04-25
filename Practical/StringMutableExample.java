06b4c4833f43bfca6fd312473093c6001ef71d02
AbiramiMohanprabu <102943522+AbiramiMohanprabu@users.noreply.github.com>
2022-04-25 12:07:56 +0530

    Add files via upload

---
+++ Practical/StringMutableExample.java
@@ -0,0 +1,23 @@
+public class StringMutableExample {
+
+   public static void my(){
+       StringBuffer s = new StringBuffer("Hello");
+	   s.append("World...");
+	   System.out.println("String is : " + s);
+	   s.insert(4, "Mamta");
+	   System.out.println("insert is " + s);
+	   
+	   StringBuilder s1 = new StringBuilder("World");
+	   s1.replace(1,3,"java");
+	   System.out.println("replace is : " + s1);
+       s1.delete(1,3);
+       System.out.println("delete is : " + s);
+        s1.reverse();		
+		System.out.println("reverse is : "+ s1);
+		
+		}
+		
+public static void main(String args[] ) {
+     my();
+	 }
+	 }
