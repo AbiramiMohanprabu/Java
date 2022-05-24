9323e06b54262dbf645f0508b50040f2563b523d
AbiramiMohanprabu <102943522+AbiramiMohanprabu@users.noreply.github.com>
2022-05-24 22:25:02 +0530

    Add files via upload

---
+++ Assignment/SubstringExmpl.java
@@ -0,0 +1,15 @@
+package Collections;
+
+public class SubstringExmpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str= new String("After all this time ? ALWAYS");
+	       System.out.println("Substring starting from index 15:");
+	       System.out.println(str.substring(15));
+	       System.out.println("Substring starting from index 15 and ending at 20:");
+	       System.out.println(str.substring(15, 20));
+	}
+
+}
