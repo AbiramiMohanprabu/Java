904a4ed9f980f50805636db4b4f43cb24b7b5469
AbiramiMohanprabu <102943522+AbiramiMohanprabu@users.noreply.github.com>
2022-05-24 13:35:29 +0530

    Add files via upload

---
+++ Assignment/GetLengthofString.java
@@ -0,0 +1,14 @@
+package Collections;
+
+public class GetLengthofString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        String str = "Crois en toi";
+		
+		int length = str.length();
+		System.out.println("The String Length of " + str + " is : " +length);
+	}
+
+}
