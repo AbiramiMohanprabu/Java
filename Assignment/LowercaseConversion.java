fa108897f72e3e4553a92583518ed1674615db42
AbiramiMohanprabu <102943522+AbiramiMohanprabu@users.noreply.github.com>
2022-05-24 22:28:28 +0530

    Add files via upload

---
+++ Assignment/LowercaseConversion.java
@@ -0,0 +1,19 @@
+package Collections;
+
+public class LowercaseConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String NAME = "ALOHOMORA";
+
+        // Convert the above string to all lowercase.
+        String lowerNAME = NAME.toLowerCase();
+
+        // Display the two strings for comparison.
+        System.out.println("Original String: " + NAME);
+        System.out.println("String in lowercase: " + lowerNAME);
+	}
+
+}
