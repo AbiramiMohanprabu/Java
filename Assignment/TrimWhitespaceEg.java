50bc7b93a275ac399dbdd6dfcc05a414ee24c297
AbiramiMohanprabu <102943522+AbiramiMohanprabu@users.noreply.github.com>
2022-05-24 22:33:08 +0530

    Add files via upload

---
+++ Assignment/TrimWhitespaceEg.java
@@ -0,0 +1,20 @@
+package Collections;
+
+public class TrimWhitespaceEg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String STR = "    Dean Castiel          ";
+
+        
+        String NEWSTR = STR.trim();
+
+
+        System.out.println("Original String: " + STR);
+
+        System.out.println("New String: " + NEWSTR);
+
+	}
+
+}
